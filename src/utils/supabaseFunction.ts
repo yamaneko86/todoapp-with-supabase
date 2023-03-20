16747188815f34fef9f24085a91d20d5f15b098f
yoshiya0448 <yoshibasket86@gmail.com>
2023-03-20 17:27:54 +0900

    ⚡️ Todoの全件取得と追加の条件にログインしているユーザーのIDを追加

--- src/utils/supabaseFunction.ts
+++ src/utils/supabaseFunction.ts
@@ -1,14 +1,14 @@
 import { supabase } from "./supabase";
 
 // Todo全件取得
-export const getAllTodos = async () => {
-  const todos = await supabase.from("todos").select("*");
+export const getAllTodos = async (user_id: string) => {
+  const todos = await supabase.from("todos").select("*").eq("user_id", user_id);
   return todos.data;
 };
 
 // Todo追加
-export const addTodo = async (title: string) => {
-  await supabase.from("todos").insert({ title: title });
+export const addTodo = async (title: string, user_id: string) => {
+  await supabase.from("todos").insert({ title: title, user_id: user_id });
 };
 
 // Todo削除
